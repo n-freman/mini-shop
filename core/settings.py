95b64a62dde2de48e2135bdd4294c8df9a84f630
n-freman <smiron1703@gmail.com>
2024-05-04 19:57:20 +0500

    setup mediafiles

--- core/settings.py
+++ core/settings.py
@@ -142,7 +142,8 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
-MEDIAFILES_DIR = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
