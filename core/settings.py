a48e60d37b445c2e027e453e9e851e6eafcdad2c
n-freman <smiron1703@gmail.com>
2024-05-06 13:00:12 +0500

    feat: add cors headers

--- core/settings.py
+++ core/settings.py
@@ -59,11 +59,13 @@ INSTALLED_APPS = [
     "django_filters",
     "drf_spectacular",
     "colorfield",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
